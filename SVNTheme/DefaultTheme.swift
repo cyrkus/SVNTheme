f6c95fa889bfdceeb7cec445bddadcee2602e96b
Aaron Bikis <aaron.bikis@gmail.com>
2017-04-06 16:04:00 -0700

    add public init to defaultTheme

--- SVNTheme/DefaultTheme.swift
+++ SVNTheme/DefaultTheme.swift
@@ -9,12 +9,23 @@
 import Foundation
 
 public struct SVNTheme_DefaultDark: SVNTheme {
-    public var navigationBarColor: UIColor = UIColor(r: 50, g: 41, b: 41)
-    public var backgroundColor: UIColor = UIColor(r: 91, g: 91, b: 81)
-    public var primaryDialogColor: UIColor = UIColor(r: 130, g: 84, b: 84)
-    public var secondaryDialogColor: UIColor = UIColor(r: 84, g: 42, b: 43)
-    public var tertiaryDialogColor: UIColor = UIColor(r: 85, g: 117, b: 130)
-    public var acceptButtonTintColor: UIColor = UIColor(r: 0, g: 198, b: 82)
-    public var declineButtonTintColor: UIColor = UIColor(r: 214, g: 0, b: 0)
-    public var stanardButtonTintColor: UIColor = UIColor(r: 255, g: 255, b: 255)
+    public var navigationBarColor: UIColor
+    public var backgroundColor: UIColor
+    public var primaryDialogColor: UIColor
+    public var secondaryDialogColor: UIColor
+    public var tertiaryDialogColor: UIColor
+    public var acceptButtonTintColor: UIColor
+    public var declineButtonTintColor: UIColor
+    public var stanardButtonTintColor: UIColor
+    
+    public init() {
+        self.navigationBarColor = UIColor(r: 50, g: 41, b: 41)
+        self.backgroundColor = UIColor(r: 91, g: 91, b: 81)
+        self.primaryDialogColor = UIColor(r: 130, g: 84, b: 84)
+        self.secondaryDialogColor = UIColor(r: 84, g: 42, b: 43)
+        self.tertiaryDialogColor = UIColor(r: 85, g: 117, b: 130)
+        self.acceptButtonTintColor = UIColor(r: 0, g: 198, b: 82)
+        self.declineButtonTintColor = UIColor(r: 214, g: 0, b: 0)
+        self.stanardButtonTintColor = UIColor(r: 255, g: 255, b: 255)
+    }
 }
