9c63e90b698b23d02b04b2be080480107f082667
Aaron Bikis <aaron.bikis@gmail.com>
2017-04-06 15:55:59 -0700

    make default theme public

--- SVNTheme/DefaultTheme.swift
+++ SVNTheme/DefaultTheme.swift
@@ -8,13 +8,13 @@
 
 import Foundation
 
-struct SVNTheme_DefaultDark: SVNTheme {
-    var navigationBarColor: UIColor = UIColor(r: 50, g: 41, b: 41)
-    var backgroundColor: UIColor = UIColor(r: 91, g: 91, b: 81)
-    var primaryDialogColor: UIColor = UIColor(r: 130, g: 84, b: 84)
-    var secondaryDialogColor: UIColor = UIColor(r: 84, g: 42, b: 43)
-    var tertiaryDialogColor: UIColor = UIColor(r: 85, g: 117, b: 130)
-    var acceptButtonTintColor: UIColor = UIColor(r: 0, g: 198, b: 82)
-    var declineButtonTintColor: UIColor = UIColor(r: 214, g: 0, b: 0)
-    var stanardButtonTintColor: UIColor = UIColor(r: 255, g: 255, b: 255)
+public struct SVNTheme_DefaultDark: SVNTheme {
+    public var navigationBarColor: UIColor = UIColor(r: 50, g: 41, b: 41)
+    public var backgroundColor: UIColor = UIColor(r: 91, g: 91, b: 81)
+    public var primaryDialogColor: UIColor = UIColor(r: 130, g: 84, b: 84)
+    public var secondaryDialogColor: UIColor = UIColor(r: 84, g: 42, b: 43)
+    public var tertiaryDialogColor: UIColor = UIColor(r: 85, g: 117, b: 130)
+    public var acceptButtonTintColor: UIColor = UIColor(r: 0, g: 198, b: 82)
+    public var declineButtonTintColor: UIColor = UIColor(r: 214, g: 0, b: 0)
+    public var stanardButtonTintColor: UIColor = UIColor(r: 255, g: 255, b: 255)
 }
