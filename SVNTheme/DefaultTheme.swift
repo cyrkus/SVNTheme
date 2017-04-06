b14702faaa660cadd78446ca21964ac2a96f1b11
Aaron Bikis <aaron.bikis@gmail.com>
2017-04-06 15:40:06 -0700

    add ghex

---
+++ SVNTheme/DefaultTheme.swift
@@ -0,0 +1,21 @@
+//
+//  DefaultTheme.swift
+//  SVNTheme
+//
+//  Created by Aaron Dean Bikis on 4/6/17.
+//  Copyright © 2017 7apps. All rights reserved.
+//
+
+import Foundation
+import HEXColor
+
+struct SVNTheme_Default: SVNTheme {
+    var navigationBarColor: UIColor
+    var backgroundColor: UIColor
+    var primaryDialogColor: UIColor = UIColor("5b5151")
+    var secondaryDialogColor: UIColor
+    var tertiaryDialogColor: UIColor
+    var acceptButtonTintColor: UIColor
+    var declineButtonTintColor: UIColor
+    var stanardButtonTintColor: UIColor
+}
