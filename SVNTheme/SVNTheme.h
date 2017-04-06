d3055cd1493652015c168ec1d271032642be4e32
Aaron Bikis <aaron.bikis@gmail.com>
2017-04-06 13:59:15 -0700

    Initial Commit

---
+++ SVNTheme/SVNTheme.h
@@ -0,0 +1,19 @@
+//
+//  SVNTheme.h
+//  SVNTheme
+//
+//  Created by Aaron Dean Bikis on 4/6/17.
+//  Copyright © 2017 7apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SVNTheme.
+FOUNDATION_EXPORT double SVNThemeVersionNumber;
+
+//! Project version string for SVNTheme.
+FOUNDATION_EXPORT const unsigned char SVNThemeVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVNTheme/PublicHeader.h>
+
+
