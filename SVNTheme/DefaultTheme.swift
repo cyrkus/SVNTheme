1d6f9e58b50d1ce70495459e730d84542330a845
Aaron Bikis <aaron.bikis@gmail.com>
2017-04-06 15:52:09 -0700

    add default theme

--- SVNTheme/DefaultTheme.swift
+++ SVNTheme/DefaultTheme.swift
@@ -7,15 +7,14 @@
 //
 
 import Foundation
-import HEXColor
 
-struct SVNTheme_Default: SVNTheme {
-    var navigationBarColor: UIColor
-    var backgroundColor: UIColor
-    var primaryDialogColor: UIColor = UIColor("5b5151")
-    var secondaryDialogColor: UIColor
-    var tertiaryDialogColor: UIColor
-    var acceptButtonTintColor: UIColor
-    var declineButtonTintColor: UIColor
-    var stanardButtonTintColor: UIColor
+struct SVNTheme_DefaultDark: SVNTheme {
+    var navigationBarColor: UIColor = UIColor(r: 50, g: 41, b: 41)
+    var backgroundColor: UIColor = UIColor(r: 91, g: 91, b: 81)
+    var primaryDialogColor: UIColor = UIColor(r: 130, g: 84, b: 84)
+    var secondaryDialogColor: UIColor = UIColor(r: 84, g: 42, b: 43)
+    var tertiaryDialogColor: UIColor = UIColor(r: 85, g: 117, b: 130)
+    var acceptButtonTintColor: UIColor = UIColor(r: 0, g: 198, b: 82)
+    var declineButtonTintColor: UIColor = UIColor(r: 214, g: 0, b: 0)
+    var stanardButtonTintColor: UIColor = UIColor(r: 255, g: 255, b: 255)
 }
