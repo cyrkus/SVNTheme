3f09a0a464961a597338441dabe1180314791709
Aaron Bikis <aaron.bikis@gmail.com>
2017-05-05 12:25:12 -0700

    add menu colors

--- SVNTheme/DefaultTheme.swift
+++ SVNTheme/DefaultTheme.swift
@@ -9,34 +9,40 @@
 import Foundation
 
 public struct SVNTheme_DefaultDark: SVNTheme {
-    public var navigationBarColor: UIColor
-    public var backgroundColor: UIColor
-    public var primaryDialogColor: UIColor
-    public var secondaryDialogColor: UIColor
-    public var tertiaryDialogColor: UIColor
-    public var acceptButtonTintColor: UIColor
-    public var declineButtonTintColor: UIColor
-    public var stanardButtonTintColor: UIColor
-    public var largeHeading: UIFont
-    public var mediumHeading: UIFont
-    public var smallHeading: UIFont
-    public var body: UIFont
-    public var textField: UIFont
-    
-    public init() {
-        self.navigationBarColor = UIColor(r: 50, g: 41, b: 41)
-        self.backgroundColor = UIColor(r: 91, g: 91, b: 81)
-        self.primaryDialogColor = UIColor(r: 130, g: 84, b: 84)
-        self.secondaryDialogColor = UIColor(r: 84, g: 42, b: 43)
-        self.tertiaryDialogColor = UIColor(r: 85, g: 117, b: 130)
-        self.acceptButtonTintColor = UIColor(r: 0, g: 198, b: 82)
-        self.declineButtonTintColor = UIColor(r: 214, g: 0, b: 0)
-        self.stanardButtonTintColor = UIColor(r: 255, g: 255, b: 255)
-        let standardFontName = "Palatino"
-        self.largeHeading = UIFont(name: standardFontName, size: 32)!
-        self.mediumHeading = UIFont(name: standardFontName, size: 26)!
-        self.smallHeading = UIFont(name: standardFontName, size: 20)!
-        self.body = UIFont(name: standardFontName, size: 20)!
-        self.textField = UIFont(name: "HelveticaNeue-Thin", size: 26)!
-    }
+  public var navigationBarColor: UIColor
+  public var backgroundColor: UIColor
+  public var primaryDialogColor: UIColor
+  public var secondaryDialogColor: UIColor
+  public var tertiaryDialogColor: UIColor
+  public var primaryMenuBarColor: UIColor
+  public var secondaryMenuBarColor: UIColor
+  public var tertiaryMenuBarColor: UIColor
+  public var acceptButtonTintColor: UIColor
+  public var declineButtonTintColor: UIColor
+  public var stanardButtonTintColor: UIColor
+  public var largeHeading: UIFont
+  public var mediumHeading: UIFont
+  public var smallHeading: UIFont
+  public var body: UIFont
+  public var textField: UIFont
+  
+  public init() {
+    self.navigationBarColor = UIColor(r: 50, g: 41, b: 41)
+    self.backgroundColor = UIColor(r: 91, g: 91, b: 81)
+    self.primaryDialogColor = UIColor(r: 130, g: 84, b: 84)
+    self.secondaryDialogColor = UIColor(r: 84, g: 42, b: 43)
+    self.tertiaryDialogColor = UIColor(r: 85, g: 117, b: 130)
+    self.primaryDialogColor = UIColor(r: 124, g: 84, b: 129)
+    self.secondaryMenuBarColor = UIColor(r: 84, g: 95, b: 130)
+    self.tertiaryDialogColor = UIColor(r: 85, g: 123, b: 130)
+    self.acceptButtonTintColor = UIColor(r: 0, g: 198, b: 82)
+    self.declineButtonTintColor = UIColor(r: 214, g: 0, b: 0)
+    self.stanardButtonTintColor = UIColor(r: 255, g: 255, b: 255)
+    let standardFontName = "Palatino"
+    self.largeHeading = UIFont(name: standardFontName, size: 32)!
+    self.mediumHeading = UIFont(name: standardFontName, size: 26)!
+    self.smallHeading = UIFont(name: standardFontName, size: 20)!
+    self.body = UIFont(name: standardFontName, size: 20)!
+    self.textField = UIFont(name: "HelveticaNeue-Thin", size: 26)!
+  }
 }
--- SVNTheme/SVNTheme.swift
+++ SVNTheme/SVNTheme.swift
@@ -10,30 +10,33 @@ import Foundation
 
 /**
  A protocol to create global SVNApps thematic colors
- Naming convention based off of Google Material design ideology 
+ Naming convention based off of Google Material design ideology
  https://material.io/guidelines/style/color.html#color-themes
  - variables :
-    - navigationBarColor
-    - backgroundColor
-    - primaryDialogColor
-    - secondaryDialogColor
-    - tertiaryDialogColor
-    - standardButtonTintColor
-    - acceptButtonTintColor
-    - declineButtonTintColor
+ - navigationBarColor
+ - backgroundColor
+ - primaryDialogColor
+ - secondaryDialogColor
+ - tertiaryDialogColor
+ - standardButtonTintColor
+ - acceptButtonTintColor
+ - declineButtonTintColor
  */
 public protocol SVNTheme {
-    var navigationBarColor: UIColor { get set }
-    var backgroundColor: UIColor { get set }
-    var primaryDialogColor: UIColor { get set }
-    var secondaryDialogColor: UIColor { get set }
-    var tertiaryDialogColor: UIColor { get set }
-    var stanardButtonTintColor: UIColor { get set }
-    var acceptButtonTintColor: UIColor { get set }
-    var declineButtonTintColor: UIColor { get set }
-    var largeHeading: UIFont { get set }
-    var mediumHeading: UIFont { get set }
-    var smallHeading: UIFont { get set }
-    var body: UIFont { get set }
-    var textField: UIFont { get set }
+  var navigationBarColor: UIColor { get set }
+  var backgroundColor: UIColor { get set }
+  var primaryDialogColor: UIColor { get set }
+  var secondaryDialogColor: UIColor { get set }
+  var tertiaryDialogColor: UIColor { get set }
+  var primaryMenuBarColor: UIColor { get set }
+  var secondaryMenuBarColor: UIColor { get set }
+  var tertiaryMenuBarColor: UIColor { get set }
+  var stanardButtonTintColor: UIColor { get set }
+  var acceptButtonTintColor: UIColor { get set }
+  var declineButtonTintColor: UIColor { get set }
+  var largeHeading: UIFont { get set }
+  var mediumHeading: UIFont { get set }
+  var smallHeading: UIFont { get set }
+  var body: UIFont { get set }
+  var textField: UIFont { get set }
 }
